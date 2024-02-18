07bc722fdd644ff5959079409bcc02dec84cf908
Aman Kumar <amankarguwal0@gmail.com>
2024-02-18 17:43:13 +0530

    redirect to login and signup functionality

--- home/script.js
+++ home/script.js
@@ -1,2 +1,19 @@
 
-let login = document.querySelector("#login")
+let login = document.querySelector("#login");
+let signup = document.querySelector("#signup");
+
+login.addEventListener("click", ()=>{
+    redirectToLogin();
+})
+signup.addEventListener("click", ()=>{
+    redirectToSignup();
+})
+
+function redirectToLogin() {
+    window.location.href = '../login/login.html';
+    sessionStorage.setItem("error", "");
+}
+function redirectToSignup() {
+    window.location.href = '../login/signup.html';
+    sessionStorage.setItem("error", "");
+}
