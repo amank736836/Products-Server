e0c21b740921f9ec02dde3bfb1a43b82ac0d110d
Aman Kumar <amankarguwal0@gmail.com>
2024-02-20 12:10:16 +0530

    home page with products added

--- home/script.js
+++ home/script.js
@@ -2,6 +2,11 @@
 let login = document.querySelector("#login");
 let signup = document.querySelector("#signup");
 
+let buttons = document.querySelector("#buttons");
+
+const div = document.querySelector("#products");
+
+
 login.addEventListener("click", ()=>{
     redirectToLogin();
 })
@@ -14,4 +19,38 @@ function redirectToLogin() {
 }
 function redirectToSignup() {
     window.location.href = '../signup/signup.html';
+}
+
+let products = [];
+
+function fetchFromLocalStorage(){
+    if(localStorage.getItem("products")!='[]' && localStorage.getItem("products")){
+        products = JSON.parse(localStorage.getItem("products"));
+        products.forEach((item) => AddtoUI(item))
+    }else{
+        buttons.setAttribute("style", "border-radius: 0cm 0cm 1cm 1cm;");
+    }
+}
+
+fetchFromLocalStorage();
+
+
+function AddtoUI(obj){
+
+    let div1 = document.createElement('div');
+    let span1 = document.createElement('span');
+    let span2 = document.createElement('span');
+    let span3 = document.createElement('span');
+
+    div1.setAttribute("id", "div1");
+
+    span1.innerText = obj.name;
+    span2.innerText = obj.desc;
+    span3.innerText = obj.price;
+
+    div1.appendChild(span1);
+    div1.appendChild(span2);
+    div1.appendChild(span3);
+    div.appendChild(div1);
+    
 }
